feb1e6d14397139be44f09a06e44298fa237d435
Lordkvas <145777821+Lordkvas@users.noreply.github.com>
2024-03-18 17:31:56 +0400

    Add files via upload

---
+++ tablica/tablica/Form1.cs
@@ -0,0 +1,286 @@
+namespace tablica
+{
+    
+    public partial class Form1 : Form
+    {   
+
+        int n = 1;
+        int[] number = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        
+        public Form1()
+
+        {
+
+            InitializeComponent();
+        }
+
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void  finish()
+        {
+            if (n == 21) label2.Visible = false;
+            if (n == 21) label3.Visible = true;
+            if (n == 21) label1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text==n.ToString())
+            if (button1.Text == n.ToString()) button1.Enabled = false;
+            else button1.Enabled = true;
+            if (button1.Text == n.ToString()) label2.Text =(n+1).ToString();
+            if (button1.Text == n.ToString()) n = n + 1;
+            finish();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == n.ToString())
+            if (button2.Text == n.ToString()) button2.Enabled = false;
+            else button2.Enabled = true;
+            if (button2.Text == n.ToString()) label2.Text =(n+1).ToString() ;
+            if (button2.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text==n.ToString())
+            if (button3.Text == n.ToString()) button3.Enabled = false;
+            else button3.Enabled = true;
+            if (button3.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button3.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text==n.ToString())
+            if (button4.Text == n.ToString()) button4.Enabled = false;
+            else button4.Enabled = true;
+            if (button4.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button4.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text==n.ToString())
+            if (button5.Text == n.ToString()) button5.Enabled = false;
+            else button5.Enabled = true;
+            if (button5.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button5.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.Text == n.ToString())
+                if (button6.Text == n.ToString()) button6.Enabled = false;
+            else button6.Enabled = true;
+            if (button6.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button6.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (button7.Text == n.ToString())
+                if (button7.Text == n.ToString()) button7.Enabled = false;
+            else button7.Enabled = true;
+            if (button7.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button7.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (button8.Text == n.ToString())
+                if (button8.Text == n.ToString()) button8.Enabled = false;
+            else button8.Enabled = true;
+            if (button8.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button8.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (button9.Text == n.ToString())
+                if (button9.Text == n.ToString()) button9.Enabled = false;
+            else button9.Enabled = true;
+            if (button9.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button9.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (button10.Text == n.ToString())
+                if (button10.Text == n.ToString()) button10.Enabled = false;
+            else button10.Enabled = true;
+            if (button10.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button10.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (button11.Text == n.ToString())
+                if (button11.Text == n.ToString()) button11.Enabled = false;
+            else button11.Enabled = true;
+            if (button11.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button11.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (button12.Text == n.ToString())
+                if (button12.Text == n.ToString()) button12.Enabled = false;
+            else button12.Enabled = true;
+            if (button12.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button12.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (button13.Text == n.ToString())
+                if (button13.Text == n.ToString()) button13.Enabled = false;
+            else button13.Enabled = true;
+            if (button13.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button13.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (button14.Text == n.ToString())
+                if (button14.Text == n.ToString()) button14.Enabled = false;
+            else button14.Enabled = true;
+            if (button14.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button14.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (button15.Text == n.ToString())
+                if (button15.Text == n.ToString()) button15.Enabled = false;
+            else button15.Enabled = true;
+            if (button15.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button15.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (button16.Text == n.ToString())
+                if (button16.Text == n.ToString()) button16.Enabled = false;
+            else button16.Enabled = true;
+            if (button16.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button16.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (button17.Text == n.ToString())
+                if (button17.Text == n.ToString()) button17.Enabled = false;
+            else button17.Enabled = true;
+            if (button17.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button17.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (button18.Text == n.ToString())
+                if (button18.Text == n.ToString()) button18.Enabled = false;
+            else button18.Enabled = true;
+            if (button18.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button18.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            if (button19.Text == n.ToString())
+                if (button19.Text == n.ToString()) button19.Enabled = false;
+            else button19.Enabled = true;
+            if (button19.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button19.Text == n.ToString()) n = n + 1;
+            finish();
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            if (button20.Text == n.ToString())
+            if (button20.Text == n.ToString()) button20.Enabled = false;
+
+            else button20.Enabled = true;
+            if (button19.Text == n.ToString()) label2.Text = (n + 1).ToString();
+            if (button20.Text == n.ToString()) n = n + 1;
+            finish();
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            Shuffle(number);
+            button1.Text = number[1].ToString();
+            button2.Text = number[2].ToString();
+            button3.Text = number[3].ToString();
+            button4.Text = number[4].ToString();
+            button5.Text = number[5].ToString();
+            button6.Text = number[6].ToString();
+            button7.Text = number[7].ToString();
+            button8.Text = number[8].ToString();
+            button9.Text = number[9].ToString();
+            button10.Text = number[10].ToString();
+            button11.Text = number[11].ToString();
+            button12.Text = number[12].ToString();
+            button13.Text = number[13].ToString();
+            button14.Text = number[14].ToString();
+            button15.Text = number[15].ToString();
+            button16.Text = number[16].ToString();
+            button17.Text = number[17].ToString();
+            button18.Text = number[18].ToString();
+            button19.Text = number[19].ToString();
+            button20.Text = number[00].ToString();
+        }
+        
+    // создаем экземпл€р класса Random дл€ генерировани€ случайных чисел
+    public static void Shuffle(int[] arr)
+        { 
+    Random rand = new Random();
+ 
+    for (int i = arr.Length - 1; i >= 1; i--)
+    {
+        int j = rand.Next(i + 1);
+
+        int tmp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = tmp;
+    }
+}
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
